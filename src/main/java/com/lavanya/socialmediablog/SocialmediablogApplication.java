b08e85ac0c0cb0f5783b8a6ad4680211fc71d9ff
Lavanya <eerisetti.lavanya@gmail.com>
2024-11-20 21:58:23 -0500

    initial commit

---
+++ src/main/java/com/lavanya/socialmediablog/SocialmediablogApplication.java
@@ -0,0 +1,13 @@
+package com.lavanya.socialmediablog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialmediablogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialmediablogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lavanya/socialmediablog/SocialmediablogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lavanya.socialmediablog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialmediablogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
