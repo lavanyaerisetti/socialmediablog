c1aa4112e24e54caceec215450fd1c103da0c8c9
Lavanya <eerisetti.lavanya@gmail.com>
2024-12-18 21:53:11 -0500

    Model mapper implemented

--- src/main/java/com/lavanya/socialmediablog/utils/CommentEntityMapper.java
+++ src/main/java/com/lavanya/socialmediablog/utils/CommentEntityMapper.java
@@ -2,29 +2,39 @@ package com.lavanya.socialmediablog.utils;
 
 import com.lavanya.socialmediablog.dto.CommentDto;
 import com.lavanya.socialmediablog.entity.CommentEntity;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class CommentEntityMapper {
+    @Autowired
+    private ModelMapper modelMapper;
 
-    public CommentDto mapCommentEntityToDto(final CommentEntity commentEntity){
-        CommentDto commentDto=new CommentDto();
-        commentDto.setId(commentEntity.getId());
-        commentDto.setUserName(commentEntity.getUserName());
-        commentDto.setEmail(commentEntity.getEmail());
-        commentDto.setBody(commentEntity.getBody());
-        return commentDto;
+    public CommentDto mapCommentEntityToDto(final CommentEntity commentEntity) {
+        if (commentEntity != null) {
+//        CommentDto commentDto=new CommentDto();
+//        commentDto.setId(commentEntity.getId());
+//        commentDto.setUserName(commentEntity.getUserName());
+//        commentDto.setEmail(commentEntity.getEmail());
+//        commentDto.setBody(commentEntity.getBody());
+            return this.modelMapper.map(commentEntity, CommentDto.class);
 
+        }
+        return null;
     }
 
-    public CommentEntity mapCommentDtoToEntity(final CommentDto commentDto){
-        CommentEntity commentEntity=new CommentEntity();
-        commentEntity.setId(commentDto.getId());
-        commentEntity.setUserName(commentDto.getUserName());
-        commentEntity.setEmail(commentDto.getEmail());
-        commentEntity.setBody(commentDto.getBody());
-        return commentEntity;
+    public CommentEntity mapCommentDtoToEntity(final CommentDto commentDto) {
+        if (commentDto != null) {
+//        CommentEntity commentEntity=new CommentEntity();
+//        commentEntity.setId(commentDto.getId());
+//        commentEntity.setUserName(commentDto.getUserName());
+//        commentEntity.setEmail(commentDto.getEmail());
+//        commentEntity.setBody(commentDto.getBody());
+            return this.modelMapper.map(commentDto, CommentEntity.class);
+
+        }
+        return null;
 
     }
-
 }
--- src/main/java/com/lavanya/socialmediablog/utils/PostEntityMapper.java
+++ src/main/java/com/lavanya/socialmediablog/utils/PostEntityMapper.java
@@ -2,28 +2,38 @@ package com.lavanya.socialmediablog.utils;
 
 import com.lavanya.socialmediablog.dto.PostDto;
 import com.lavanya.socialmediablog.entity.PostEntity;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class PostEntityMapper {
+    @Autowired
+    private ModelMapper modelMapper;
 
     //map or convert post entity to post dto
     public PostDto mapPostEntityToPostDto(final PostEntity postEntity) {
-        PostDto postDto = new PostDto();
-        postDto.setId(postEntity.getId());
-        postDto.setTitle(postEntity.getTitle());
-        postDto.setDescription(postEntity.getDescription());
-        postDto.setContent(postEntity.getContent());
-        return postDto;
+        if (postEntity != null) {
+//        PostDto postDto = new PostDto();
+//        postDto.setId(postEntity.getId());
+//        postDto.setTitle(postEntity.getTitle());
+//        postDto.setDescription(postEntity.getDescription());
+//        postDto.setContent(postEntity.getContent());
+            return this.modelMapper.map(postEntity, PostDto.class);
+        }
+        return null;
     }
 
         // map or convert post dto to post entity
         public PostEntity mapPostDtoToPostEntity(final PostDto postDto) {
-            PostEntity postEntity  = new PostEntity();
-            postEntity.setTitle(postDto.getTitle());
-            postEntity.setDescription(postDto.getDescription());
-            postEntity.setContent(postDto.getContent());
-            return postEntity;
+            if (postDto != null) {
+//            PostEntity postEntity  = new PostEntity();
+//            postEntity.setTitle(postDto.getTitle());
+//            postEntity.setDescription(postDto.getDescription());
+//            postEntity.setContent(postDto.getContent());
+                return this.modelMapper.map(postDto, PostEntity.class);
+            }
+            return null;
         }
     }
 
